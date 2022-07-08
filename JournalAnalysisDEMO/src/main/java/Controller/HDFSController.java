543f31b28443c22c10707012bff7ca68b8a1642e
bander972 <1628580708@qq.com>
2022-07-08 10:14:48 +0800

    HDFS version 1.0
    there is unfinished part including unwritten paths,unfinished methods

---
+++ JournalAnalysisDEMO/src/main/java/Controller/HDFSController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+import service.HDFSService;
+
+@Controller
+public class HDFSController {
+    @Autowired
+    public HDFSService hdfsService;
+
+
+    @PostMapping(value="/upload")
+    public void getFile(MultipartFile file){
+    String path="hdfs://input";
+    }
+}
