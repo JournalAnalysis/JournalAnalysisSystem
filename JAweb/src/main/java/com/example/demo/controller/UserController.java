7ef64218a41a13e11e99bc034f56d4991551a54d
cjh0186 <leo3344244@126.com>
2022-07-08 16:22:48 +0800

    后端

--- JAweb/src/main/java/com/example/demo/controller/UserController.java
+++ JAweb/src/main/java/com/example/demo/controller/UserController.java
@@ -3,15 +3,22 @@ package com.example.demo.controller;
 import com.example.demo.Repository.CompanyRepository;
 import com.example.demo.Repository.UserRepository;
 import com.example.demo.entity.Company;
+import com.example.demo.entity.UploadFileResponse;
 import com.example.demo.entity.User;
+import com.example.demo.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private FileService fileService;
 
     @Autowired
     UserRepository userRepository;
@@ -36,5 +43,19 @@ public class UserController {
         return "修改成功！";
     }
 
+    //上传单个文件
+    @RequestMapping("/upload/file")
+    public UploadFileResponse uploadFile(
+            @RequestParam("file") MultipartFile file){
+
+        String fileName = fileService.storeFile(file);
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        return new UploadFileResponse(fileName, fileDownloadUri,
+                file.getContentType(), file.getSize());
+    }
 
 }
