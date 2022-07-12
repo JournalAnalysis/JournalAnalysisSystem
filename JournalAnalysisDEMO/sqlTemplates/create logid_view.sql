754ce637c47f9642de9499252182ade5f60008c3
give-to <1139019079@qq.com>
2022-07-12 10:23:51 +0800

    修改创建视图模板文件

--- JournalAnalysisDEMO/sqlTemplates/create logid_view.sql
+++ JournalAnalysisDEMO/sqlTemplates/create logid_view.sql
@@ -1,25 +1,30 @@
 -- 	总访问人次（PV）
+DROP VIEW IF EXISTS logid_pv;
 CREATE VIEW logid_pv ( pv_count ) AS SELECT
 max( id ) 
 FROM
 	logid;
 -- 访问人数
+DROP VIEW IF EXISTS logid_uv;
 CREATE VIEW logid_uv ( uv_count ) AS SELECT
 count( id ) 
 FROM
 	logid_first;
 -- 独立ip
+DROP VIEW IF EXISTS logid_dist_ip;
 CREATE VIEW logid_dist_ip ( dist_ip ) AS SELECT
 count( DISTINCT ip ) 
 FROM
 	logid_first;
 -- 访问链接Top 10
+DROP VIEW IF EXISTS logid_top10;
 CREATE VIEW logid_top10 AS SELECT
 * 
 FROM
 	logid_url_top;
 	
 -- 各个时间访问人次
+DROP VIEW IF EXISTS logid_access_time;
 CREATE VIEW logid_access_time (
 	access_hour,
 num) AS SELECT
@@ -31,6 +36,7 @@ GROUP BY
 	access_hour;
 
 -- 访问来源占比
+DROP VIEW IF EXISTS logid_ref_type;
 CREATE VIEW logid_ref_type ( 
 ref_type, VALUE ) AS SELECT
 ref_type,
@@ -51,6 +57,7 @@ HAVING
 -- 	logid_ip_black;
 	
 -- 地域分布logid_prov
+DROP VIEW IF EXISTS logid_prov;
 CREATE VIEW logid_prov ( province, count ) AS SELECT
 province,
 count( id ) 
@@ -60,6 +67,7 @@ GROUP BY
 	province;
 	
 -- 操作系统占比
+DROP VIEW IF EXISTS logid_os;
 CREATE VIEW logid_os AS 
 SELECT
 client_type,
@@ -70,6 +78,7 @@ GROUP BY
 	client_type;
 	
 -- 浏览器占比
+DROP VIEW IF EXISTS logid_brow;
 CREATE VIEW logid_brow AS 
 SELECT
 client_browser,
@@ -80,6 +89,7 @@ GROUP BY
 	client_browser;
 	
 -- 网络运营商占比
+DROP VIEW IF EXISTS logid_isp;
 CREATE VIEW logid_isp AS 
 SELECT
 isp,
@@ -90,6 +100,7 @@ GROUP BY
 	client_browser;
 	
 -- 搜索引擎占比
+DROP VIEW IF EXISTS logid_srch_eng;
 CREATE VIEW logid_srch_eng ( NAME, count ) AS SELECT
 ref_type,
 count( ref_type )
